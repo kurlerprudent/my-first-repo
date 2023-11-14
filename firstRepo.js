eb14bb39092629b4d82497809b31a16f60f5adc8
kurlerprudent <149204888+kurlerprudent@users.noreply.github.com>
2023-11-14 08:03:46 +0000

    classes
    class animal description

--- firstRepo.js
+++ firstRepo.js
@@ -1,3 +1,15 @@
 'use strict'
 let myName = "Obed Sarkodie";
 console.log("Hello " + myName);
+
+class Animal{
+    constructor(domestic, wild){
+        this.domestic = domestic;
+        this.wild = wild;
+    }
+    showName(){
+        console.log("This animal is "+ this.wild);
+    }
+}
+let ani = new Animal("LIOn", "goat");
+ani.showName();
